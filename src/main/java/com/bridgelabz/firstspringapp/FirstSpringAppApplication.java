f8659c5aefc43b39ce5ff2b51b56fe030378a705
govind6498 <govindsingh6498@gmail.com>
2021-11-19 16:16:16 +0530

    Added:To Display Hello World

---
+++ src/main/java/com/bridgelabz/firstspringapp/FirstSpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Govind Kumar,How are You?");
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+		
+	}
+}
